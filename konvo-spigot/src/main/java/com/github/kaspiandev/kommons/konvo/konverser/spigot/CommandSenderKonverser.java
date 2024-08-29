b1f9481122098377437205442822bd4f749b3c7f
KaspianDev <kacperleaguenine@gmail.com>
2024-08-29 14:37:04 +0200

    Add component messages, support multiple Konvesers in CommandSenderKonverser

--- konvo-spigot/src/main/java/com/github/kaspiandev/kommons/konvo/konverser/spigot/CommandSenderKonverser.java
+++ konvo-spigot/src/main/java/com/github/kaspiandev/kommons/konvo/konverser/spigot/CommandSenderKonverser.java
@@ -17,26 +17,57 @@
 
 package com.github.kaspiandev.kommons.konvo.konverser.spigot;
 
+import com.github.kaspiandev.kommons.konvo.konverser.StringKonverser;
 import com.github.kaspiandev.kommons.konvo.message.Message;
 import net.md_5.bungee.api.chat.BaseComponent;
 import org.bukkit.command.CommandSender;
 
-public class CommandSenderKonverser implements ComponentKonverser<CommandSender> {
+public class CommandSenderKonverser {
 
     private final CommandSender entity;
+    private final CommandSenderComponentKonverser componentKonverser;
+    private final CommandSenderStringKonverser stringKonverser;
 
     public CommandSenderKonverser(CommandSender entity) {
         this.entity = entity;
+        this.componentKonverser = new CommandSenderComponentKonverser();
+        this.stringKonverser = new CommandSenderStringKonverser();
     }
 
-    @Override
-    public CommandSender getEntity() {
-        return entity;
+    public CommandSenderComponentKonverser getAsComponent() {
+        return componentKonverser;
     }
 
-    @Override
-    public void message(Message<BaseComponent[]> message) {
-        entity.spigot().sendMessage(message.getContents());
+    public CommandSenderStringKonverser getAsString() {
+        return stringKonverser;
+    }
+
+    public class CommandSenderComponentKonverser implements ComponentKonverser<CommandSender> {
+
+        @Override
+        public CommandSender getEntity() {
+            return entity;
+        }
+
+        @Override
+        public void message(Message<BaseComponent[]> message) {
+            entity.spigot().sendMessage(message.getContents());
+        }
+
+    }
+
+    public class CommandSenderStringKonverser implements StringKonverser<CommandSender> {
+
+        @Override
+        public CommandSender getEntity() {
+            return entity;
+        }
+
+        @Override
+        public void message(Message<String> message) {
+            entity.sendMessage(message.getContents());
+        }
+
     }
 
 }
